d9f2bf7066b4c045261b3ac1f8eda4f28f5ffec7
Kornthip <sang.kornthip@gmail.com>
2021-03-20 17:44:59 -0400

    animate burger icon :tada: :dancers:

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //declare variable for icon and  menu list
 const burgerMenuList = document.querySelector('nav.burgerMenu ul');
-const burgerIcon = document.querySelector('nav.burgerMenu i')
+const burgerIcon = document.querySelector('nav.burgerMenu .burgerIcon')
 
 burgerIcon.addEventListener('click', function() {
+    //make menu list appear
     burgerMenuList.classList.toggle('addHeight');
+
+    //make burgerIcon become x
+    burgerIcon.classList.toggle('animateBurgerIcon');
 });
