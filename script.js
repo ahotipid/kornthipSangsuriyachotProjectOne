7bd7e7d28d5667a246e7fd32f9ca67eba32348fd
Kornthip <sang.kornthip@gmail.com>
2021-03-20 18:47:14 -0400

    debugging a tag

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const burgerMenuList = document.querySelector('nav.burgerMenu ul');
 const burgerIcon = document.querySelector('nav.burgerMenu .burgerIcon')
 
 burgerIcon.addEventListener('click', function() {
-    //make menu list appear
+    //make menu list height appear
     burgerMenuList.classList.toggle('addHeight');
 
     //make burgerIcon become x
